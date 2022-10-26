0ee88ae00bf63e0b37d0c8edc53615d65fd699cf
Floyd <Floydwmcdonald@gmail.com>
2022-10-26 14:38:08 -0400

    intermediate commit

--- src/Main/ClackClient.java
+++ src/Main/ClackClient.java
@@ -114,7 +114,6 @@ public class ClackClient {
      * or nothing.
      */
     public void readClientData() {
-
         while (inFromStd.hasNext()){
             String userInput = inFromStd.next();
             if (userInput == "DONE"){
